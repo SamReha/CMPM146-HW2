924662e679c6f0a88ff3dfc25fee57563e170f84
SamReha <sreha@ucsc.edu>
2015-04-12 20:22:56 -0700

    Improved UTC bot. Now slightly worse than greedy bot

--- utc_bot.py
+++ utc_bot.py
@@ -1,93 +1,53 @@
-class Node:
-    """ A node in the game tree. Note wins is always from the viewpoint of player.
-        Crashes if state not specified.
-    """
-    def __init__(self, move = None, parent = None, state = None):
-        self.move = move # the move that got us to this node - "None" for the root node
-        self.parentNode = parent # "None" for the root node
-        self.childNodes = []
-        self.wins = 0
-        self.visits = 0
-        self.possibleMoves = state.get_moves() # future child nodes
-        self.player = state.get_whose_turn() # the only part of the state that the Node needs later
-        
-    def UCTSelectChild(self):
-        """ Use the UCB1 formula to select a child node. Often a constant UCTK is applied so we have
-            lambda c: c.wins/c.visits + UCTK * sqrt(2*log(self.visits)/c.visits to vary the amount of
-            exploration versus exploitation.
-        """
-        s = sorted(self.childNodes, key = lambda c: c.wins/c.visits + sqrt(2*log(self.visits)/c.visits))[-1] #c.wins/c.visits should be reward function
-        return s
+import time
+import random
+import math
+import node
+
+def think(rootstate, quip):
+    # Returns a float representing the difference between the score of me and the other player
+    def result(scoreDict, me):
+        if me == 'red':
+            return float(scoreDict['red'] - scoreDict['blue'])
+        else: # me == 'blue'
+            return float(scoreDict['blue'] - scoreDict['red'])
     
-    def AddChild(self, m, s):
-        """ Remove m from possibleMoves and add a new child node for this move.
-            Return the added child node
-        """
-        n = Node(move = m, parent = self, state = s)
-        self.possibleMoves.remove(m)
-        self.childNodes.append(n)
-        return n
-    
-    def Update(self, result):
-        """ Update this node - one additional visit and result additional wins. result must be from the viewpoint of player.
-        """
-        self.visits += 1
-        self.wins += result
-
-    def __repr__(self):
-        return "[M:" + str(self.move) + " W/V:" + str(self.wins) + "/" + str(self.visits) + " U:" + str(self.possibleMoves) + "]"
-
-    def TreeToString(self, indent):
-        s = self.IndentString(indent) + str(self)
-        for c in self.childNodes:
-             s += c.TreeToString(indent+1)
-        return s
-
-    def IndentString(self,indent):
-        s = "\n"
-        for i in range (1,indent+1):
-            s += "| "
-        return s
-
-    def ChildrenToString(self):
-        s = ""
-        for c in self.childNodes:
-             s += str(c) + "\n"
-        return s
-
-def utc_bot(rootstate, quip, verbose = False):
-    """ Conduct a UCT search for itermax iterations starting from rootstate.
+    """ Conduct a UCT search for at most 1 second starting from rootstate.
         Return the best move from the rootstate.
-        Assumes 2 alternating players (player 1 starts), with game results in the range [0.0, 1.0]."""
+        Never rolls out beyond the MAX_DEPTH"""
     rootnode = Node(state = rootstate)
-    startTime = gmtime()
+    me = rootstate.get_whos_turn()
+    rollouts = 0
+    startTime = time.clock()
 
-    while gmtime() - startTime < 1:
+    while time.clock() - startTime <= 1.0:
         node = rootnode
-        state = rootstate.Clone()
+        state = rootstate.copy()
 
         # Select
         while node.possibleMoves == [] and node.childNodes != []: # node is fully expanded and non-terminal
             node = node.UCTSelectChild()
-            state.DoMove(node.move)
+            state.apply_move(node.move)
 
         # Expand
         if node.possibleMoves != []: # if we can expand (i.e. state/node is non-terminal)
             m = random.choice(node.possibleMoves) 
-            state.DoMove(m)
+            state.apply_move(m)
             node = node.AddChild(m,state) # add child and descend tree
 
-        # Rollout - this can often be made orders of magnitude quicker using a state.GetRandomMove() function
+        # Rollout
         while state.get_moves() != []: # while state is non-terminal
-            state.DoMove(random.choice(state.get_moves()))
+            state.apply_move(random.choice(state.get_moves()))
+            rollouts = rollouts+1
 
         # Backpropagate
         while node != None: # backpropagate from the expanded node and work back to the root node
-            node.Update(state.GetResult(node.player)) # state is terminal. Update node with result from POV of node.player
+            node.Update(result(state.get_score(), me)) # state is terminal. Update node with result from POV of me.
             node = node.parentNode
+    bestChild = sorted(rootnode.childNodes, key = lambda c: c.visits)[-1] # Get the child that was most visited
+    
+    # Output some information about my thought process.
+    quip("I think the best move is " + str(bestChild.move) + ' with an expected score: ' + str(bestChild.score))
+    quip("In less than 1 second, I preformed " + str(rollouts) + " rollouts.")
+    quip(rootnode.ChildrenToString())
 
-    # Output some information about the tree - can be omitted
-    if (verbose): print rootnode.TreeToString(0)
-    else: print rootnode.ChildrenToString()
-
-    return sorted(rootnode.childNodes, key = lambda c: c.visits)[-1].move # return the move that was most visited
+    return bestChild.move
